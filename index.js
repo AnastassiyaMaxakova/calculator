e88677835544793e245545702066e94ff4a8e322
AnastassiyaMaxakova <anastassiya.maxakova@gmail.com>
2024-03-25 20:05:31 +0500

    change html and css, add empty functions of calculator, comments

--- index.js
+++ index.js
@@ -1,31 +1,47 @@
-let display = document.getElementById('display');
+const display = document.getElementById('display');
 
 
 
-function appendToDisplay(input){
-    display.value = input; 
+
+function appendToDisplay(element){
+    display.value += element; 
+}
+
+function add(a,b){
+    let result = a+b;
+    return result;
+}
+
+function subtract(a,b){
+    let result = a-b;
+    return result;
+}
+
+function multiply(a,b){
 
 }
 
-function add(){
+function divide(a,b){
 
 }
 
-function subtract(){
-
-}
-
-function multiply(){
-
-}
-
-function divide(){
-
-}
-
-function operate(){
+function operate(input){
     
 }
-function clear(){
-    display.textContent = 'dddd';
+
+function clearDisplay(){
+    display.value = '';
+
 }
+
+//let string = '234+345+232-569555/455*85';
+//const expression = string.split(/([+*/-])/g) 
+
+/*write a function that:
+1. takes a first element of expression
+2. checks an operator
+3. based on an operator does on of operation functions 
+4. updates the result with that evaluated expression
+5. start the loop again till the end of the array
+6. return a final evaluated result
+*/
