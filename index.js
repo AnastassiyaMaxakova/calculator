11e5f4574bbad55c6272253963775ee36ef6e680
AnastassiyaMaxakova <anastassiya.maxakova@gmail.com>
2024-03-25 17:11:12 +0500

    change display type and css styling

---
+++ index.js
@@ -0,0 +1,31 @@
+let display = document.getElementById('display');
+
+
+
+function appendToDisplay(input){
+    display.value = input; 
+
+}
+
+function add(){
+
+}
+
+function subtract(){
+
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+function operate(){
+    
+}
+function clear(){
+    display.textContent = 'dddd';
+}
